47a31da68309a3c5e82b8b699793fa8847ea515d
jayantahalder8 <javawithjayanta@gmail.com>
2024-04-07 13:37:16 +0530

    background video added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
+    <div>
+      <Main />
+    </div>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
