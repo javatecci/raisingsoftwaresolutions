682a60b81aa992f3958be7ea378e0b0969b1f675
jayantahalder8 <javawithjayanta@gmail.com>
2024-04-07 17:13:52 +0530

    background gh-page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,34 @@ import React from 'react'
 import videoBg from '../assets/videoBg.mp4'
 
 
+
 const Main = () => {
   return (
     <div className='main'>
         <div className="overlay"></div>
+
+
+
+
         <video src={videoBg} autoPlay loop muted />
         <div className="content">
+
+        <div className="button">
+
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+<a href='https://javatecci.github.io/client/'>me</a>
+
+
+</div>
+
             <h1>Welcome</h1>
             <p>To my site. I am Jayanta</p>
 
@@ -18,7 +40,7 @@ const Main = () => {
         </p>
         <a href='https://javatecci.github.io/client/'
         >
-          If you Like this website You may Contact me .
+        me
         </a>
 
         </div>
