51c3bc3bf0df9da98fde6bc5a78960e13f321234
jayantahalder8 <javawithjayanta@gmail.com>
2024-04-07 14:32:13 +0530

    link other apps also

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
 
@@ -9,20 +9,7 @@ function App() {
 
     <div>
       <Main />
-    </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        </div>
       </header>
     </div>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
+///import logo from './logo.svg';
 import React from 'react'
 import videoBg from '../assets/videoBg.mp4'
+
 
 const Main = () => {
   return (
@@ -8,7 +10,22 @@ const Main = () => {
         <video src={videoBg} autoPlay loop muted />
         <div className="content">
             <h1>Welcome</h1>
-            <p>To my site.</p>
+            <p>To my site. I am Jayanta</p>
+
+
+        <p>
+          If you Like this website You may Contact me .
+        </p>
+        <a href='https://javatecci.github.io/raisingsoftwaresolution/'
+        >
+          If you Like this website You may Contact me .
+        </a>
+
+        <a href='https://javatecci.github.io/client/'
+        >
+          If you Like this website You may Contact me .
+        </a>
+
         </div>
     </div>
   )
